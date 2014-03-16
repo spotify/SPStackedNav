ada17638188f4e44a8b7630a50bc03c7fc892f5d
Joachim Bengtsson <nevyn@spotify.com>
2014-03-16 18:36:50 +0000

    Switch license to Apache 2.0

--- Examples/StackExample/StackExample/RootTestViewController.h
+++ Examples/StackExample/StackExample/RootTestViewController.h
@@ -1,10 +1,18 @@
+// Copyright 2014 Spotify
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
-//  RootTestViewController.h
-//  StackExample
-//
-//  Created by Joachim Bengtsson on 2012-10-27.
-//  Copyright (c) 2012 Spotify. All rights reserved.
-//
+//  File created by Joachim Bengtsson on 2012-10-27.
 
 #import <UIKit/UIKit.h>
 
